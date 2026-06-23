296c6b815287fd66ebe0c8fac4562d634c4e5479
mgulaydr-svg <mgulaydr@gmail.com>
2026-06-23 23:30:11 +0300

    firebase ve cloudinary yazıldı

---
+++ src/services/cloudinary.js
@@ -0,0 +1,19 @@
+export const uploadToCloudinary = async (file) => {
+    const cloudName = "YOUR_CLOUDINARY_CLOUD_NAME";
+    const uploadPreset = "YOUR_UNSIGNED_PRESET_NAME"; // Önceki projelerdeki preset
+    const formData = new FormData();
+    formData.append("file", file);
+    formData.append("upload_preset", uploadPreset);
+    try {
+        const response = await fetch(`https://api.cloudinary.com/v1_1/${cloudName}/upload`, {
+        method: "POST",
+        body: formData
+        });
+        if (!response.ok) throw new Error("Yükleme hatası oluştu.");
+        const data = await response.json();
+        return data.secure_url; // Cloudinary'den dönen güvenli bağlantı
+    } catch (error) {
+        console.error("Cloudinary Upload Error:", error);
+        throw error;
+    }
+};
